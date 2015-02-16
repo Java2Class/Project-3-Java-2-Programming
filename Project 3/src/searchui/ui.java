78a22e4db3b423c708d381d5984d1d2cf204f374
Scrizz <mainbone@gmail.com>
2015-02-16 12:05:38 -0500

    dded gitignore

---
+++ Project 3/src/searchui/ui.java
@@ -0,0 +1,19 @@
+/*
+ * Search engine UI class for COP-2805
+ * Contains all the parts of the user interface
+ */
+package searchui;
+
+/**
+ *
+ * @author Jair Garcia-Varela
+ */
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*; // quick and easy pop-ups
+
+class UI {
+    UI (){
+        JOptionPane.showMessageDialog(null,"This is a test popup"); //testing to see if imports worked
+    }
+}
