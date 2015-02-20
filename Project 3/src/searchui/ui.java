07758ae5a57621b19fc6d505f0cbd8cf23985b21
John Silvey <johnsilvey@gmail.com>
2015-02-20 08:46:55 -0500

    Added a search bar with search button Added radio buttons to select which kind of search the user wants to perform Placed search bar and buttons in a panel at the top of the window Added search listener to top search button

--- Project 3/src/searchui/ui.java
+++ Project 3/src/searchui/ui.java
@@ -54,7 +54,55 @@ class UI {
             buttons.add(exitButton);
             window1.add(buttons, BorderLayout.SOUTH);
         // end of button creation
-                 
+            
+        //**************************************************
+        //Created panel for search bar with button 
+        JPanel pSearchBar = new JPanel();
+        pSearchBar.setLayout(new FlowLayout(FlowLayout.CENTER, 10,10));
+        
+        //Create and add label, textbox, and search button to panel 
+        JLabel jlblSearch = new JLabel("Enter Terms to Search:");
+        JTextField jtfSearch = new JTextField(30);
+        JButton jbtnSearch = new JButton("Search");
+        jbtnSearch.addActionListener(new searchListener());
+        
+        pSearchBar.add(jlblSearch);
+        pSearchBar.add(jtfSearch);
+        pSearchBar.add(jbtnSearch);
+        
+        //************************************************************
+        //Create Panel For Buttons
+        JPanel pRadioButtons = new JPanel();
+        pRadioButtons.setLayout(new FlowLayout(FlowLayout.CENTER, 10,10));        
+        //Create Radio Buttons For Panel
+        JRadioButton jrbAll = new JRadioButton("Search ALL Terms");
+        JRadioButton jrbAny = new JRadioButton("Search ANY Term", true);
+        JRadioButton jrbExact = new JRadioButton("Search Exact Phrase");
+        //Add Radio Buttons To Group
+        ButtonGroup bgSearch = new ButtonGroup();
+        bgSearch.add(jrbAny);
+        bgSearch.add(jrbAll);
+        bgSearch.add(jrbExact);          
+        //Add Radio Buttons to Panel        
+        pRadioButtons.add(jrbAny);
+        pRadioButtons.add(jrbAll);
+        pRadioButtons.add(jrbExact);
+        
+        //********************************************************
+        //Create Panel to Combine Search Bar and Search Button Panels
+        JPanel pSearch = new JPanel();
+        pSearch.setLayout(new BorderLayout());
+        //add both panels to this panel
+        pSearch.add((pSearchBar), BorderLayout.NORTH);
+        pSearch.add((pRadioButtons),BorderLayout.CENTER);
+        
+        //*********************************************************
+        //Add Search Panel to Main Window in North Position
+        window1.add((pSearch), BorderLayout.NORTH);
+        
+        
+        
+        
             
         window1.setVisible(true);
         
