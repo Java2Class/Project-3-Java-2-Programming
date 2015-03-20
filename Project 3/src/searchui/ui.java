df7681be077da95c3a12a540753b88ccc14e8fd0
Haven <juddhaven@verizon.net>
2015-03-19 20:36:02 -0400

    I added as many options to the "file" and "help" sections in the menu bar as I can think of. If I need to add some or rearrange some, I can do that. Also, I was adding a Directions or Help page. I started working on it, but I had difficulty on how to make it. I wanted to write a text and import that to the page simply without coding it down in our program, but everything I did failed, so I'll work on the Directions page later. Also I had to duplicate some information like the logo, because for some reason it wouldn't let me use what was already there. It come up as blank, so it worked when I copied the coding and added "2" to it like "logo2".

--- Project 3/src/searchui/ui.java
+++ Project 3/src/searchui/ui.java
@@ -20,6 +20,7 @@ import java.io.*;
 import java.util.*;
 import java.applet.*; //search sound
 import java.net.*; //search sound
+import java.nio.file.*;
 
 class UI {
     /**
@@ -27,9 +28,9 @@ class UI {
      * @param s the absolute path of the index
      * @param mIndex map that stores the file names and path names
      */
-    UI(String s, Map mIndex) {
+    UI(String s, Map mIndex) throws FileNotFoundException, IOException {
         //main window for search UI
-        Frame window1 = new Frame("eXtreme Team Search");
+        JFrame window1 = new JFrame("eXtreme Team Search");
         window1.setSize(960,600);
         window1.setLocationRelativeTo(null);
         
@@ -77,7 +78,12 @@ class UI {
             JButton clearButton = new JButton("Clear Fields");
             clearButton.setToolTipText("Clears entered fields.");
             clearButton.setPreferredSize(new Dimension(110, 27));
-
+            clearButton.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    JOptionPane.showMessageDialog(window1, "Clear Button Operational.");
+                }
+            });
+            
             JButton exitButton = new JButton("Exit");
             exitButton.setToolTipText("Exits the program.");
             exitButton.setPreferredSize(new Dimension(110, 27));
@@ -171,6 +177,144 @@ class UI {
         jrbExact.setBackground(Color.WHITE);
         buttons.setBackground(Color.WHITE);
         pSearchBar.setBackground(Color.WHITE);
+        
+        //Directions Frame
+        JFrame window2 = new JFrame("eXtreme Team Search Directions");
+        window2.setSize(960,600);
+        window2.setLocationRelativeTo(null);
+        
+        ImageIcon image2 = new ImageIcon(getClass().getResource("../images/EXTS.png"));
+        JLabel logo2 = new JLabel(image2);
+        
+        window2.setIconImage(image2.getImage());
+        
+        JPanel buttons2 = new JPanel();
+            buttons2.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
+        
+            JButton okButton = new JButton("OK");
+            okButton.setToolTipText("Exit the Directions menu.");
+            okButton.setPreferredSize(new Dimension(110, 27));
+            okButton.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){      
+                    window2.dispose();
+                }
+            });
+        buttons2.add(okButton);    
+        
+        JPanel dTitle = new JPanel();
+        dTitle.setLayout(new FlowLayout(FlowLayout.CENTER, 10,10));
+            
+        JLabel dText = new JLabel("Directions:"); 
+        JLabel dCore = new JLabel("Here will go the instructions.");
+      
+        dTitle.add(dText, BorderLayout.NORTH);
+        dTitle.add(dCore, BorderLayout.SOUTH);
+        window2.add(logo2, BorderLayout.NORTH);
+        window2.add(dTitle, BorderLayout.CENTER);
+        window2.add(buttons2, BorderLayout.SOUTH);
+         
+        //MenuBar Setup
+        JMenuBar menuBar = new JMenuBar();
+        
+            //Menu Item 1 - File
+            JMenu file = new JMenu("File");
+            file.setMnemonic(KeyEvent.VK_F);
+              
+                //Add Files Menu
+                JMenuItem addIndexMenu = new JMenuItem("Add");
+                addIndexMenu.setMnemonic(KeyEvent.VK_A);
+                addIndexMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, 
+                        ActionEvent.CTRL_MASK));
+                addIndexMenu.setToolTipText("Allows you to add files to the index.");
+                addIndexMenu.addActionListener(new ActionListener(){
+                    public void actionPerformed(ActionEvent e){      
+                        AddFile.addFile(s, mIndex, aList);
+                    }
+                }); 
+                addIndexMenu.setHorizontalTextPosition(JMenuItem.RIGHT);
+                file.add(addIndexMenu);
+        
+                //Remove Files Menu
+                JMenuItem removeIndexMenu = new JMenuItem("Remove");
+                removeIndexMenu.setMnemonic(KeyEvent.VK_R);
+                removeIndexMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, 
+                        ActionEvent.CTRL_MASK));
+                removeIndexMenu.setToolTipText("Allows you to remove a file from the index.");
+                removeIndexMenu.addActionListener(new ActionListener(){
+                    public void actionPerformed(ActionEvent e){
+                        RemoveFile.removeFile(mIndex, aList);
+                    }
+                });
+                removeIndexMenu.setHorizontalTextPosition(JMenuItem.RIGHT);
+                file.add(removeIndexMenu);
+                       
+                //Update Menu     
+                JMenuItem updateIndexMenu = new JMenuItem("Update");
+                updateIndexMenu.setMnemonic(KeyEvent.VK_U);
+                updateIndexMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_U, 
+                        ActionEvent.CTRL_MASK));
+                updateIndexMenu.setToolTipText("Allows you to update the index.");
+                updateIndexMenu.addActionListener(new ActionListener(){
+                    public void actionPerformed(ActionEvent e){
+                        JOptionPane.showMessageDialog(window1, "Update Menu Operational.");
+                    }
+                });
+                updateIndexMenu.setHorizontalTextPosition(JMenuItem.RIGHT);
+                file.add(updateIndexMenu);
+                
+                //Clear
+                JMenuItem clearIndexMenu = new JMenuItem("Clear");
+                clearIndexMenu.setMnemonic(KeyEvent.VK_C);
+                clearIndexMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, 
+                        ActionEvent.CTRL_MASK));
+                clearIndexMenu.setToolTipText("Clears entered fields.");
+                clearIndexMenu.addActionListener(new ActionListener(){
+                    public void actionPerformed(ActionEvent e){
+                        JOptionPane.showMessageDialog(window1, "Clear Menu Operational.");
+                    }
+                });
+                clearIndexMenu.setHorizontalTextPosition(JMenuItem.RIGHT);
+                file.add(clearIndexMenu);
+                
+                //Exit
+                JMenuItem quitMenu = new JMenuItem("Quit");
+                quitMenu.setMnemonic(KeyEvent.VK_Q);
+                quitMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Q, 
+                        ActionEvent.CTRL_MASK));
+                quitMenu.setToolTipText("Exits the program.");
+                quitMenu.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e){
+                        System.exit(0);
+                    }
+                });
+                quitMenu.setHorizontalTextPosition(JMenuItem.RIGHT);
+                file.add(quitMenu);
+            //end of Menu Item 1  
+            
+            //Menu Item 2 - Help
+            JMenu help = new JMenu("Help");
+            help.setMnemonic(KeyEvent.VK_H); 
+            
+                //Directions
+                JMenuItem dirMenu = new JMenuItem("Directions");
+                dirMenu.setMnemonic(KeyEvent.VK_D);
+                dirMenu.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_D, 
+                        ActionEvent.CTRL_MASK));
+                dirMenu.setToolTipText("Opens the Directions window.");
+                dirMenu.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e){
+                        window2.setVisible(true);
+                    }
+                });
+                dirMenu.setHorizontalTextPosition(JMenuItem.RIGHT);
+                help.add(dirMenu);
+            //end of Menu Item 2 
+                
+        //adding menus to menubar        
+        menuBar.add(file);
+        menuBar.add(help);
+        window1.setJMenuBar(menuBar);
+        
         
         //*********************************************************
         //Add Search Panel to Main Window in Center Position and logo in North
