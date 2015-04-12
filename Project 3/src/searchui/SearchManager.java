2cc1bede0f8b715ec70febaff72c525117d23970
Jason Andrew Kerby <jason.a.kerby@gmail.com>
2015-04-12 15:23:54 -0400

    Added some skeleton methods inside the SearchManager -Kerb

--- Project 3/src/searchui/SearchManager.java
+++ Project 3/src/searchui/SearchManager.java
@@ -11,7 +11,7 @@ package searchui;
  */
 public class SearchManager {
     
-        /**
+     /**
      *
      * @param userInput A string containing the text entered in the UI text field
      * @param searchType An int which determines the type of search you want to do:
@@ -22,5 +22,37 @@ public class SearchManager {
     static void determineSearch(String userInput, int searchType){
         System.out.println(userInput);
         System.out.println(searchType);
+        
+        String normalizedString = normalizeString(userInput);
+        
+        if(searchType == -1)
+            allSearch(normalizedString);
+        else if(searchType == 0)
+            anySearch(normalizedString);
+        else
+            exactSearch(normalizedString);
+            
     } 
+    
+    //normalizes the string entered by the user
+    static String normalizeString(String userInput){
+        
+        return "Test";
+    }
+    
+    //performs an all search
+    static void allSearch(String s){
+        
+    }
+    
+    //performs an any search
+    static void anySearch(String s){
+        
+    }
+    
+    //performs an exact search
+    static void exactSearch(String s){
+        
+    }
+    
 }
