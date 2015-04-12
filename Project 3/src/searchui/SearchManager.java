54fea30bbf3789965ec3315e5eee60755b8fac4d
Jason Andrew Kerby <jason.a.kerby@gmail.com>
2015-04-12 16:33:42 -0400

    More work on SearchManager, including an input normalizer and passing aList to the methods of SearchManager.  This will allow us to utilize the files in the index. -Kerb

--- Project 3/src/searchui/SearchManager.java
+++ Project 3/src/searchui/SearchManager.java
@@ -5,6 +5,12 @@
  */
 package searchui;
 
+import java.text.*;
+import java.util.Locale;
+import java.util.*;
+import java.io.*;
+
+
 /**
  *
  * @author Jason Kerby
@@ -18,40 +24,44 @@ public class SearchManager {
      *                   -1: An ALL search
      *                    0: An ANY search
      *                    1: An EXACT search
+     * @param aList An array list used to identify files in the index for searching
      */
-    static void determineSearch(String userInput, int searchType){
-        System.out.println(userInput);
-        System.out.println(searchType);
+    static void determineSearch(String userInput, int searchType, ArrayList<String> aList){
+    //TESTING//   System.out.println(userInput);
+    //TESTING//    System.out.println(searchType);
         
         String normalizedString = normalizeString(userInput);
-        
-        if(searchType == -1)
-            allSearch(normalizedString);
-        else if(searchType == 0)
-            anySearch(normalizedString);
-        else
-            exactSearch(normalizedString);
-            
-    } 
+    //TESTING//    System.out.println(normalizedString);  
     
+    //TESTING//    System.out.println(aList);
+
+        if(searchType == -1)
+            allSearch(normalizedString, aList);
+        else if(searchType == 0)
+            anySearch(normalizedString, aList);
+        else
+            exactSearch(normalizedString,aList);         
+    } 
+   
     //normalizes the string entered by the user
     static String normalizeString(String userInput){
-        
-        return "Test";
+        userInput = Normalizer.normalize(userInput, Normalizer.Form.NFKC);
+    //TESTING//    userInput = userInput.replaceAll("[^\\p{IsAlphabetic}\\p{Digit}]", "");   
+        return userInput;
     }
     
     //performs an all search
-    static void allSearch(String s){
+    static void allSearch(String s, ArrayList<String> aList){
         
     }
     
     //performs an any search
-    static void anySearch(String s){
+    static void anySearch(String s, ArrayList<String> aList){
         
     }
     
     //performs an exact search
-    static void exactSearch(String s){
+    static void exactSearch(String s, ArrayList<String> aList){
         
     }
     
